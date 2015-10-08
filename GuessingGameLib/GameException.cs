11b854e67a21882b5445c43c373c829163d9375f
AshwiniNaidu <ashwininaidu.23@gmail.com>
2015-10-08 10:34:41 +0530

    Game tests using .NET TDD

---
+++ GuessingGameLib/GameException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuessingGameLib
+{
+    public class GameException : ApplicationException
+    {
+        public GameException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
+
